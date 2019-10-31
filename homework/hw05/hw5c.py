fe42ee001a6652b2b5a5381fcb15be3e1f216156
JakeNat <50184492+JakeNat@users.noreply.github.com>
2019-10-31 15:04:46 -0400

    Delete hw5c.py

--- homework/hw05/hw5c.py
+++
@@ -1,8 +0,0 @@
-# File: hw5c.py
-# Author: Jake Natalizia
-# Date: October 30th, 2019
-# Section: B
-# Email: jacob.natalizia@maine.edu
-# Description: 
-# Collaboration: I did not collaborate with anyone.
-
